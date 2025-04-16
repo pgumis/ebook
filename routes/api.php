02af7476c1c78e3c03e6b013c8b060bc359eba3c
Pawel Gumiela <pgumiela35@gmail.com>
2025-04-16 19:16:54 +0200

    Commit 2

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function () {
+    return view('home');
+});
+
 Route::post('/rejestracja', [AuthController::class, 'rejestracja']);
 
 Route::post('/logowanie', [AuthController::class, 'logowanie']);
