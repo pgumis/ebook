f9a3231def9f1b10a758ee056e4f65b73d1401a3
Pawel <pgumiela35@gmail.com>
2025-06-08 19:39:50 +0200

    .

--- frontend/src/components/BooksList/BooksListFilterPanel/BooksListFilterPanel.jsx
+++ frontend/src/components/BooksList/BooksListFilterPanel/BooksListFilterPanel.jsx
@@ -1,22 +1,36 @@
 const BooksListFilterPanel = () => {
     return <div className="panel">
-        <h3>Filtrowanie</h3>
-        <h5 style={{marginBottom: '0'}}>Kategorie</h5>
-        <ul style={{padding:'1rem', margin:0}}>
-            <li>biografie</li>
-            <li>biznes</li>
-            <li>dla dzieci</li>
-            <li>dla młodzieży</li>
-            <li>fantastyka</li>
-            <li>historia</li>
-            <li>horror</li>
-            <li>komiks</li>
-            <li>literatura faktu</li>
-            <li>literatura obyczajowa</li>
-            <li>rozwój osobisty</li>
-            <li>sport</li>
-            <li>turystyka</li>
+        <h3 style={{marginBottom: '0'}}>Kategorie</h3>
+        <br></br>
+        <ul style={{listStyle: "none", padding: 0, margin: 0}}>
+            <li><a href="">Atlasy i albumy</a></li>
+            <li><a href="">Biografie</a></li>
+            <li><a href="">Biznes</a></li>
+            <li><a href="">Dla dzieci</a></li>
+            <li><a href="">Fantastyka</a></li>
+            <li><a href="">Historia</a></li>
+            <li><a href="">Horror</a></li>
+            <li><a href="">Informatyka</a></li>
+            <li><a href="">Komiksy</a></li>
+            <li><a href="">Kryminał, sensacja, thriller</a></li>
+            <li><a href="">Kuchnia</a></li>
+            <li><a href="">Kultura i sztuka</a></li>
+            <li><a href="">Lektury</a></li>
+            <li><a href="">Literatura faktu</a></li>
+            <li><a href="">Literatura piękna</a></li>
+            <li><a href="">Literatura popularnonaukowa</a></li>
+            <li><a href="">Nauka języków</a></li>
+            <li><a href="">Nauki ścisłe, medycyna</a></li>
+            <li><a href="">Podróże i turystyka</a></li>
+            <li><a href="">Poezja i dramat</a></li>
+            <li><a href="">Poradniki</a></li>
+            <li><a href="">Prasa</a></li>
+            <li><a href="">Prawo</a></li>
+            <li><a href="">Religia</a></li>
+            <li><a href="">Romanse</a></li>
+            <li><a href="">Sport</a></li>
         </ul>
+
     </div>
 }
 export default BooksListFilterPanel;
